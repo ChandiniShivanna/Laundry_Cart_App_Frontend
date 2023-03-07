2f7c868a1b4db74742e6958675ecfa599dfbacc1
Chandini S <120159560+ChandiniShivanna@users.noreply.github.com>
2023-03-07 15:23:34 +0530

    Update summary.jsx

--- src/componants/order/Summary/summary.jsx
+++ src/componants/order/Summary/summary.jsx
@@ -124,7 +124,7 @@ export default function Summary(props){
                 // console.log(completeData);
                 async function postCall() {
                   try {
-                    let url ="https://laudry-cart.onrender.com" + "/createorder";
+                    let url ="https://laundry-backend-9l08.onrender.com" + "/createorder";
                     await axios(url, {
                       method: "post",
                       data: completeData,
